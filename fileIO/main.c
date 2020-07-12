b2e9875c097547a58e3755a50a8a9126745818a3
njazz25 <winny92255@gmail.com>
2020-07-13 04:01:46 +0800

    exit(1) later fprint

--- fileIO/main.c
+++ fileIO/main.c
@@ -1,1 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+  int num;
+  File *fptr;
+  ftpr = fopen("file1.txt", "w");
+  if(ftpr == NULL){
+    printf("ERROR!");
+    exit(1);
+  }
+  printf("Enter number: ");
+  scanf("%d", &num);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
